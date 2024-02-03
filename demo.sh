368e29aceef0807f77cbc700e82060cefc513000
Chad Dombrova <chadrik@gmail.com>
2024-02-02 17:23:20 -0800

    Make it more clear when beta development starts

--- demo.sh
+++ demo.sh
@@ -5,10 +5,8 @@ cmtz() {
   PYTHONPATH=~/dev/commitizen python3.8 -m commitizen "$@"
 }
 
-git_tag() {
-  local tag=''
-  tag=$(cmtz bump "$@" --dry-run | grep tag | sed 's/tag to create: \(.*\)/\1/')
-  git tag "$tag"
+get_tag() {
+  echo $(cmtz bump "$@" --dry-run | grep tag | sed 's/tag to create: \(.*\)/\1/')
 }
 
 git checkout master
@@ -21,62 +19,68 @@ git tag "1.0.0"
 
 # create develop
 git checkout -b develop
+cmtz version -p
+git commit --allow-empty -m "develop: Starting beta development for 1.1"
+git tag $(get_tag --prerelease beta --increment MINOR --force-prerelease)
 
 # add a feature to develop
 touch src/feat1.txt
 git add src/feat1.txt
 git commit -m "develop: add beta feature1"
-git_tag --prerelease beta --increment MINOR --force-prerelease
+git tag $(get_tag --prerelease beta)
 
 # create staging
 git checkout -b staging
 cmtz version -p
-git commit --allow-empty -m "staging: Starting release candidate"
-git_tag --prerelease rc --increment PATCH
+git commit --allow-empty -m "staging: Starting release candidate for 1.1"
+git tag $(get_tag --prerelease rc --increment PATCH)
 
 # add another feature to develop
 git checkout develop
+git commit --allow-empty -m "develop: Starting beta development for 1.2"
+git tag $(get_tag --prerelease beta --increment MINOR --force-prerelease)
+
 touch src/feat2.txt
 git add src/feat2.txt
 git commit -m "develop: add beta feature2"
-git_tag --prerelease beta --increment MINOR --force-prerelease
+git tag $(get_tag --prerelease beta --increment PATCH)
 
 # add a hotfix to master
 git checkout master
 touch src/fix.txt
 git add src/fix.txt
 git commit -m "master: add hotfix"
-git_tag --increment PATCH
+git tag $(get_tag --increment PATCH)
 
 # merge the hotfix to staging
 git checkout staging
-git merge master -m "auto-merge hotfix from master into staging"
-git_tag --prerelease rc --increment PATCH
+git merge master -m "staging: auto-merge hotfix from master"
+git tag $(get_tag --prerelease rc --increment PATCH)
 
 # merge the hotfix to develop
 git checkout develop
-git merge staging -m "auto-merge hotfix from staging into develop"
-git_tag --prerelease beta --increment PATCH
+git merge staging -m "develop: auto-merge hotfix from staging"
+git tag $(get_tag --prerelease beta --increment PATCH)
 
 # add a feature fix to staging
 git checkout staging
 echo "more awesome" >> src/feat1.txt
 git add src/feat1.txt
 git commit -m "staging: update beta feature"
-git_tag --prerelease rc --increment PATCH
+git tag $(get_tag --prerelease rc --increment PATCH)
 
 # merge the hotfix to develop
 git checkout develop
-git merge staging -m "auto-merge rc hotfix from staging into develop"
-git_tag --prerelease beta --increment PATCH
+git merge staging -m "develop: auto-merge rc hotfix from staging"
+git tag $(get_tag --prerelease beta --increment PATCH)
 
 # Release time!
 # merge staging to master
 echo "Releasing develop to master!"
 git checkout master
 git merge staging -m "Release develop to master"
-git commit --allow-empty -m "New release!"
-git_tag --increment PATCH
+git commit --allow-empty -m "New release! 1.1"
+git tag $(get_tag --increment PATCH)
 
 ## merge the hotfix to staging
 #git checkout staging
@@ -88,18 +92,17 @@ git_tag --increment PATCH
 #git merge staging -m "auto-merge hotfix from staging into develop"
 #git_tag --prerelease beta --increment PATCH
 
-## develop becomes release candidate
+# develop becomes release candidate
 echo "Converting develop branch into release candidate"
 git checkout staging
 git reset --hard develop
 #git merge develop -m "make new release candidate"
-git commit --allow-empty -m "staging: Starting release candidate"
-git_tag --prerelease rc --increment PATCH
+git commit --allow-empty -m "staging: Starting release candidate for 1.2"
+git tag $(get_tag --prerelease rc --increment PATCH)
 
 # start a new beta cycle
-#echo "Setting up new develop branch for beta development"
-#git checkout develop
-#git reset --hard master
-##git merge staging -m "make new develop branch"
-#git commit --allow-empty -m "develop: Starting branch"
-#git_tag --prerelease beta --increment MINOR --force-prerelease
+echo "Setting up new develop branch for beta development"
+git checkout develop
+#git merge staging -m "make new develop branch"
+git commit --allow-empty -m "develop: Starting beta development for 1.3"
+git tag $(get_tag --prerelease beta --increment MINOR --force-prerelease)
