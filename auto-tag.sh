881b37389f61cb0ac3b3a3c2050f3fd1ad874cd3
Chad Dombrova <chadrik@gmail.com>
2024-02-03 18:38:44 -0800

    Use patch not minor

--- auto-tag.sh
+++ auto-tag.sh
@@ -15,12 +15,7 @@ echo "$CI_REPOSITORY_URL"
 
 git remote add gitlab_origin https://oauth2:$ACCESS_TOKEN@gitlab.com/chadrik/semver-demo.git
 
-git fetch gitlab_origin --tags
-echo "TAGS"
-git tag
-echo ""
-
-
 tag=$(get_tag $TAG_ARGS)
+echo "Creating new tag $tag"
 git tag "$tag"
 git push gitlab_origin "$tag" -o ci.skip
